c4f8efe3f14e64a1e0bd3b8fac80ce490f8b57b4
Hakkology <yildizhakan88@gmail.com>
2023-10-08 15:58:15 +0300

    Errors corrected.

--- WebSocketApp/Program.cs
+++ WebSocketApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddSingleton<WebSocketModel>();
+builder.Services.AddTransient<WebSocketHandler>();
 
 var app = builder.Build();
 
