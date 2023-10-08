4eb7d2d83bea4a948900d4d344b3ddd2d26d9cba
Hakkology <yildizhakan88@gmail.com>
2023-10-08 16:32:51 +0300

    Console app connected to the websocket.

--- WebSocketApp/Models/WebSocketHandler.cs
+++ WebSocketApp/Models/WebSocketHandler.cs
@@ -15,24 +15,30 @@ namespace WebSocketApp
 
         public async Task Handle(HttpContext context, Func<Task> next)
         {
-            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            _webSocketModel.WebSocket = webSocket;
-
-            var buffer = new byte[1024 * 4];
-            while (webSocket.State == WebSocketState.Open)
+            if(context.WebSockets.IsWebSocketRequest)
             {
-                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                _webSocketModel.WebSocket = webSocket;
 
-                if (result.MessageType == WebSocketMessageType.Close)
+                var buffer = new byte[1024 * 4];
+                while (webSocket.State == WebSocketState.Open)
                 {
-                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-                    break;
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                        break;
+                    }
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, true, CancellationToken.None);
                 }
-
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, true, CancellationToken.None);
             }
-
-            await next();
+            else
+            {
+                // Only invoke next middleware when it's not a WebSocket request to allow the pipeline to continue
+                await next();
+            }
         }
     }
 }
---
+++ WebSocketConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace WebSocketConsole{
+    class Program{
+
+        // construction of the websocket
+        static System.Net.WebSockets.ClientWebSocket ClientWB = new System.Net.WebSockets.ClientWebSocket();
+
+        // 
+        static async Task Main(string[] args){
+
+            // Connection string
+            await ClientWB.ConnectAsync(new Uri("ws://localhost:5098/ws"), CancellationToken.None);
+
+            // task runner to await sending async response
+            if (ClientWB.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                await ClientWB.SendAsync(System.Text.Encoding.ASCII.GetBytes("WebSocket is working and connected through Console app."),
+                                         System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+
+            await Receive();
+            Console.ReadKey();
+
+        }
+
+        static async Task Receive(){
+            
+            bool off = false;
+            while (!off)
+            {
+                if (ClientWB.State == System.Net.WebSockets.WebSocketState.Open)
+                {
+                    byte[] bt = new byte[1024];
+                    var result = await ClientWB.ReceiveAsync(bt, CancellationToken.None);
+                    Console.WriteLine(System.Text.Encoding.ASCII.GetString(bt, 0, result.Count));
+                }
+            }
+        }
+    }
+}
